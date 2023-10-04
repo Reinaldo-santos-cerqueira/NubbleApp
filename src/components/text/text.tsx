4fc2b5e1cf637badb0986997b1d470296f34a80c
Reinaldo Manguinho <reinaldodevcontato@gmail.com>
2023-10-04 18:01:40 -0300

    feat: create for a component Text and fontSizesOptions

--- src/components/text/text.tsx
+++ src/components/text/text.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import {Text as RnText} from 'react-native'
-const Text = () =>{
-    return(
-        <></>
-    )
+import {Text as RNText, TextProps} from 'react-native';
+
+export function Text({children, ...rest}: TextProps){
+	return <RNText {...rest}>{children}</RNText>;
 }
-export {Text}
+
+const  fontSize = {
+	headingLarge:{
+		fontSize: 32,
+		lineHeight:38.4
+	},
+	headingMedium:{
+		fontSize: 22,
+		lineHeight:26.4
+	},
+	headingSmall:{
+		fontSize: 18,
+		lineHeight:23.4
+	},
+};
