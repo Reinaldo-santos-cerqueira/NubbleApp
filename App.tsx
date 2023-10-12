63a5c4dce67334a561df680aa78ad00f51644e7c
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-10-12 17:41:35 -0300

    feat: button added to home screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { Text } from './src/components/text/text';
+import { Button } from './src/components/button/button';
 const App = () =>{
 	return(
 		<SafeAreaView>
-			<Text>
-        CoffStack
+			<Text preset='headingLarge' italic>
+				CoffStack
 			</Text>
+			<Button title='Entrar'/>
 		</SafeAreaView>
 		
 	);
