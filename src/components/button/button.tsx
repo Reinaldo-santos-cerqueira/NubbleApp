1df64b7575db190b9f04ad266ba630b05ade908a
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-10-12 18:06:36 -0300

    feat: add theme for a button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { TouchableOpacity,TouchableOpacityProps as TouOpaProps } from 'react-native';
 import { Text } from '../text/text';
+import { useTheme } from '@shopify/restyle';
+import { Theme } from '../../theme/theme';
 
 interface TouchableOpacityProps extends TouOpaProps {
     title: string
 }
 
-export function Button({ title,...rest }: TouchableOpacityProps) {
+export function Button({ title, ...rest }: TouchableOpacityProps) {
+	const { colors } = useTheme<Theme>();
+
 	return (
 		<TouchableOpacity
 			style={{
 				paddingHorizontal: 20,
 				paddingVertical: 15,
-				backgroundColor: '#074C4E',
+				backgroundColor: colors.primary,
 				alignItems: 'center',
-				borderRadius: 16
+				borderRadius: 16	
 			}}
 			{...rest}
 		>
-			<Text  bold color='#FFF'>{title}</Text>
+			<Text style={{color: colors.grayWhite}}  bold>{title}</Text>
 		</TouchableOpacity>
 	);
 }
