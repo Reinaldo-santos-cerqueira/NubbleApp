9d554a4dd9f2b23e2310c585f694f47e21598f17
Reinaldo Manguinho <reinaldodevcontato@gmail.com>
2023-10-17 17:21:53 -0300

    feat: add box for button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { TouchableOpacity,TouchableOpacityProps as TouOpaProps } from 'react-native';
+import { 
+	TouchableOpacityProps as TouOpaProps 
+} from 'react-native';
 import { Text } from '../text/text';
 import { useTheme } from '@shopify/restyle';
 import { Theme } from '../../theme/theme';
+import { Box } from '../box/box';
 
 interface TouchableOpacityProps extends TouOpaProps {
     title: string
@@ -12,17 +15,21 @@ export function Button({ title, ...rest }: TouchableOpacityProps) {
 	const { colors } = useTheme<Theme>();
 
 	return (
-		<TouchableOpacity
-			style={{
-				paddingHorizontal: 20,
-				paddingVertical: 15,
-				backgroundColor: colors.primary,
-				alignItems: 'center',
-				borderRadius: 16	
-			}}
-			{...rest}
+		<Box
+			backgroundColor='greenPrimary'
+			paddingHorizontal='s20'
+			paddingVertical='s10'
+			borderRadius='s12'
+			// style={{
+			// 	paddingHorizontal: 20,
+			// 	paddingVertical: 15,
+			// 	backgroundColor: colors.primary,
+			// 	alignItems: 'center',
+			// 	borderRadius: 16	
+			// }}
+			//{...rest}
 		>
 			<Text color={colors.grayWhite} bold>{title}</Text>
-		</TouchableOpacity>
+		</Box>
 	);
 }
