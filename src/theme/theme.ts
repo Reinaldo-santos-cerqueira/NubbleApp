0540cc5002635a41bac577a26e8f5ae008e3b20f
Reinaldo Manguinho <reinaldodevcontato@gmail.com>
2023-10-17 17:19:18 -0300

    feat: created spacing and border values props for theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -24,23 +24,38 @@ export const theme = createTheme({
 		...palette,
 		primary: palette.greenPrimary,
 		primaryContrast: palette.grayWhite,
-        
+
 		buttonPrimary: palette.greenPrimary,
 
 		background: palette.grayWhite,
 		backgroundContranst: palette.grayBlack,
-        
+
 		error: palette.redError,
 		errorLight: palette.redErrorLight,
-        
+
 		sucess: palette.greenSuccess,
 		sucessLight: palette.greenSuccessLight,
-        
+
 	},
-	spacing: {},
-	borderRadius: {
-        
-	}
+	spacing: {
+		s4: 4,
+		s8: 8,
+		s10: 10,
+		s12: 12,
+		s14: 14,
+		s16: 16,
+		s20: 20,
+		s24: 24,
+		s32: 32,
+		s40: 40,
+		s48: 48,
+		s56: 56,
+	},
+	borderRadii: {
+		s8: 8,
+		s12: 12,
+		s16: 16,
+	},
 });
 
 export type Theme = typeof theme;
