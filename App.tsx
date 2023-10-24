df2508d5ac460323a7734965c054710e1e2e79d3
Reinaldo Manguinho <reinaldodevcontato@gmail.com>
2023-10-24 11:01:24 -0300

    feat: init layout off login screen

--- App.tsx
+++ App.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, TextInput, View } from 'react-native';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
 import { Box } from './src/components/box/box';
 import { Icon } from './src/components/icon/icon';
+import { Text } from './src/components/text/text';
+import { Button } from './src/components/button/button';
 
 function App(): JSX.Element{
 	return(
 		<ThemeProvider theme={theme}>
 			<SafeAreaView>
 				<View style={{paddingHorizontal: 20}}>
-					<Box flexDirection="row">
-						<Icon name="chevronRight" size={50} />
-						<Icon name="heartFill" size={50} color="buttonPrimary" />
-						<Icon name="profile" size={50} />
-						<Icon name="profileFill" size={50} />
-						<Icon name="heart" size={50} />
-						<Icon name="bellOn" color="carrotSecondary" size={50} />
+					<Text 
+						preset='headingLarge'
+						margin='s8'
+					>
+						Olá
+					</Text>
+					<Text 
+						preset='paragraphLarge'
+						bold
+						marginBottom='s40'
+					>
+						Digite seu e-mail e senha para entrar
+					</Text>
+					<Box mb='s20'>
+						<TextInput placeholder='Digite seu email' style={{ borderWidth: 1,height:50}}/>
 					</Box>
-					<Box flexDirection="row">
-						<Icon name="newPost" size={50} />
-						<Icon name="camera" size={50} />
-						<Icon name="chat" size={50} />
-						<Icon name="chatOn" color="error" size={50} />
-						<Icon name="flashOff" size={50} />
-						<Icon name="flashOn" size={50} />
+					<Box>
+						<TextInput placeholder='Digite sua senha' style={{ borderWidth: 1,height:50}}/>
 					</Box>
+					<Text color='primary' bold preset='paragraphSmall'>
+						Esqueci minha senha
+					</Text>
+					<Button mt='s48' preset='primary' title='Entrar'/>
+					<Button mt='s12' preset='outline' title='Criar conta'/>
 				</View>
 			</SafeAreaView>
 		</ThemeProvider>
