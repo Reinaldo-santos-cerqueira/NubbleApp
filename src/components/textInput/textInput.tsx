1030a6bdb962870c7b8638cb1dba2cd6c8bb3f17
Reinaldo Manguinho <reinaldodevcontato@gmail.com>
2023-10-24 11:35:38 -0300

    feat: create textInputStyle with props style of RNTextInput, add pressable, add ref for click all element focus input, create textInputContainer for style to box and configuration RNTextInput

--- src/components/textInput/textInput.tsx
+++ src/components/textInput/textInput.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import { TextInput as RNTextInput, TextInputProps as RnTextInputProps } from 'react-native';
+import React, { useRef } from 'react';
+import { Pressable, TextInput as RNTextInput, TextInputProps as RnTextInputProps, TextStyle } from 'react-native';
 import { Box, BoxProps } from '../box/box';
-import { Text } from '../text/text';
+import { $fontFamily, Text,$fontSizes } from '../text/text';
+import { useAppTheme } from '../../hooks/useAppTheme';
 
 interface TextInputProps extends RnTextInputProps {
     label: string
 }
 
 export function TextInput({label,...props}: TextInputProps){
-	return( 
-		<Box>
-			<Text mb={'s4'} preset='paragraphMedium'>{label}</Text>
-			<Box {...$textInputContainer}
-				
-			>
-				<RNTextInput {...props}/>
+	const {colors} = useAppTheme();
+	const inputRef = useRef<RNTextInput>(null);
+	function focusInput(){
+		inputRef.current?.focus();
+	}
+	return(
+		<Pressable
+			onPress={focusInput}
+		>
+			<Box>
+				<Text mb={'s4'} preset='paragraphMedium'>{label}</Text>
+				<Box 
+					{...$textInputContainer}
+				>
+					<RNTextInput 
+						ref={inputRef}
+						placeholderTextColor={colors.gray2}
+						{...props} 
+						style={$textInputStyle}
+					/>
+				</Box>
 			</Box>
-		</Box>
+		</Pressable>
 	);
 }
+
+const $textInputStyle: TextStyle = {
+	padding: 0,
+	fontFamily: $fontFamily.regular,
+	...$fontSizes.paragraphMedium
+};
 
 const $textInputContainer: BoxProps = {
 	padding:'s16',
