11140e9ea2cc3d1f6a55a4a9b5c8c283ea622eaf
Reinaldo Manguinho <reinaldodevcontato@gmail.com>
2023-10-24 17:35:22 -0300

    feat: add dinamic padding

--- src/components/screen/screen.tsx
+++ src/components/screen/screen.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Box } from '../box/box';
 import { SafeAreaView } from 'react-native';
+import { useAppSafeArea } from '../../hooks/useAppSafeArea';
 interface ScreenProps {
     children: React.ReactNode
 }
 export function Screen({children}:ScreenProps) {
+	const {top} = useAppSafeArea();
 	return(
 		<SafeAreaView>
-			<Box paddingHorizontal='s24'>
+			<Box paddingHorizontal='s24' style={{paddingTop: top}}>
 				{children}
 			</Box>
 		</SafeAreaView>
