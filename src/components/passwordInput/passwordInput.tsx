558ccf15f3945b88227a99878c2b63c38864fa74
Reinaldo Manguinho <reinaldodevcontato@gmail.com>
2023-10-25 11:11:20 -0300

    feat: create passwordInput components

---
+++ src/components/passwordInput/passwordInput.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { TextInput, TextInputProps } from '../textInput/textInput';
+import { Icon } from '../icon/icon';
+
+type PassswordInputProps = TextInputProps
+export function InputPassword(props:PassswordInputProps){
+	const [isSecurityEntry,setIsSecurityEntry] = useState(true);
+
+	function toogleSecureTextEntry(){
+		setIsSecurityEntry(!isSecurityEntry);
+	}
+    
+	return(
+		<TextInput
+			{...props}
+			secureTextEntry={isSecurityEntry}
+			icon={
+
+				<Icon onPress={toogleSecureTextEntry} name={isSecurityEntry ?'eyeOn' : 'eyeOff'} color='gray2' />
+			}
+		/>
+	);
+}
