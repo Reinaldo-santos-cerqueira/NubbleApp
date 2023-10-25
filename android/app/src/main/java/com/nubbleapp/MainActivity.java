2b271ed88ee75e113029070d15bc8e526ab6e240
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-10-25 15:23:21 -0300

    chore: config for a react navigation

--- android/app/src/main/java/com/nubbleapp/MainActivity.java
+++ android/app/src/main/java/com/nubbleapp/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,10 +12,17 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "NubbleApp";
   }
+
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
