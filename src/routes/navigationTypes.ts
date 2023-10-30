50292f368845cb08dab99fc566dd83b31777bfcf
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-10-30 11:39:57 -0300

    feat: global props to RootParamList

---
+++ src/routes/navigationTypes.ts
@@ -0,0 +1,8 @@
+import { RootStackParams } from './routes';
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace ReactNavigation {
+        interface RootParamList extends RootStackParams {}
+    }
+}
