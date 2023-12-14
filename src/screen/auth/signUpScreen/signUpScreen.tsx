dc5325c8456c43b7333205431487f1f67c03af2c
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-12-14 17:27:28 -0300

    feat: added logic for react hook form to page

--- src/screen/auth/signUpScreen/signUpScreen.tsx
+++ src/screen/auth/signUpScreen/signUpScreen.tsx
@@ -5,10 +5,26 @@ import { TextInput } from '../../../components/textInput/textInput';
 import { Button } from '../../../components/button/button';
 import { InputPassword } from '../../../components/passwordInput/passwordInput';
 import { useResetNavigation } from '../../../hooks/useResetNavigationSucess';
+import { Controller, useForm } from 'react-hook-form';
+
+type SignUpForm = {
+	email: string,
+	password: string,
+	username: string,
+	fullName: string
+}
 
 export function SignUpScreen(){
 	const {reset} = useResetNavigation();
-
+	const { control, formState, handleSubmit } = useForm<SignUpForm>({
+		defaultValues: {
+			email: '',
+			password: '',
+			username: '',
+			fullName: ''	
+		},
+		mode: 'onChange'
+	});
 	const submitForm = () => {
 		reset({
 			title: 'Sua conta foi criada com sucesso!',
@@ -25,28 +41,94 @@ export function SignUpScreen(){
 			<Text bold color='grayBlack' preset='headingLarge' mb='s32'>
                 Criar uma conta
 			</Text>
-			<TextInput 
-				label='Seu username' 
-				placeholder='@' 
-				boxProps={{mb: 's16'}}
+			<Controller
+				name={'username'}
+				control={control}
+				rules={{
+					required: 'Username é obrigatório',
+				}}
+				render={({ field, fieldState }) => {
+					return (
+						<TextInput
+							erroMessage={fieldState.error?.message}
+							value={field.value}
+							onChangeText={field.onChange}
+							label='Seu username'
+							placeholder='@'
+							boxProps={{ mb: 's16' }}
+						/>
+					);
+				}}
 			/>
-			<TextInput 
-				label='Nome completo' 
-				placeholder='Digite seu nome completo' 
-				boxProps={{mb: 's16'}}
-			/> 
-			<TextInput 
-				label='E-mail' 
-				placeholder='Digite seu e-mail' 
-				boxProps={{mb: 's16'}}
-			/> 
-			<InputPassword 
-				label='Senha' 
-				placeholder='Digite sua senha' 
-				boxProps={{mb: 's48'}}
+			<Controller
+				name={'fullName'}
+				control={control}
+				rules={{
+					required: 'Username é obrigatório',
+				}}
+				render={({ field, fieldState }) => {
+					return (
+						<TextInput
+							erroMessage={fieldState.error?.message}
+							value={field.value}
+							onChangeText={field.onChange}
+							label='Nome completo'
+							placeholder='Digite seu nome completo'
+							boxProps={{ mb: 's16' }}
+						/> 
+					);
+				}}
 			/>
+
+			<Controller
+				name={'email'}
+				control={control}
+				rules={{
+					required: 'E-mail é obrigatório',
+					pattern: {
+						message: 'E-mail deve ser valido',
+						value: /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/
+					}
+				}}
+				render={({ field, fieldState }) => {
+					return (
+						<TextInput
+							erroMessage={fieldState.error?.message}
+							value={field.value}
+							onChangeText={field.onChange}
+							label='E-mail'
+							placeholder='Digite seu email'
+							boxProps={{ mb: 's20' }}
+						/>
+					);
+				}}
+			/>
+			<Controller
+				name={'password'}
+				rules={{
+					required: 'Senha é obrigatório',
+					pattern: {
+						message: 'Senha deve ser forte',
+						value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+					}
+				}}
+				control={control}
+				render={({ field, fieldState }) => {
+					return (
+						<InputPassword
+							erroMessage={fieldState.error?.message}
+							value={field.value}
+							onChangeText={field.onChange}
+							label='Senha'
+							placeholder='Digite sua senha'
+							boxProps={{ mb: 's10' }}
+						/>
+					);
+				}}
+			/>	
 			<Button 
-				onPress={submitForm}
+				disabled={!formState.isValid}
+				onPress={handleSubmit(submitForm)}
 				title='Criar minha conta' 
 				preset='primary'
 			/>
