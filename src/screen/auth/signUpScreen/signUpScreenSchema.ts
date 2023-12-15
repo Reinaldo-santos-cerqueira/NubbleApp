557403a73d058625dd1a0d480f5b3f6cc72fc323
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-12-15 12:05:54 -0300

    feat: created schemesignup to form sign up

---
+++ src/screen/auth/signUpScreen/signUpScreenSchema.ts
@@ -0,0 +1,12 @@
+import {z} from 'zod';
+
+const userNameRegex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/gim;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
+export const signUpSchema = z.object({
+	username: z.string().regex(userNameRegex, 'Username invalid').min(5,'Deve possuir no minimo 5'),
+	fullName: z.string().min(5, 'Muito pequeno mínimo 5').max(50, 'Muito grande no máximo 50'),
+	email: z.string().email('E-mail invalido '),
+	password: z.string().regex(passwordRegex,'Senha deverá ser forte')
+});
+
+export type SignUpSchema = z.infer<typeof signUpSchema>
