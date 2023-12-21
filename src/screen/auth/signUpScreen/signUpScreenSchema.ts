4921039f126a3c04522452352cc506db51a8483e
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-12-21 16:47:35 -0300

    feat: add function transform captitalize

--- src/screen/auth/signUpScreen/signUpScreenSchema.ts
+++ src/screen/auth/signUpScreen/signUpScreenSchema.ts
@@ -1,10 +1,11 @@
 import {z} from 'zod';
+import { stringUtils } from '../../../utils/stringFunctions';
 
 const userNameRegex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/gim;
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
 export const signUpSchema = z.object({
 	username: z.string().regex(userNameRegex, 'Username invalid').min(5,'Deve possuir no minimo 5'),
-	fullName: z.string().min(5, 'Muito pequeno mínimo 5').max(50, 'Muito grande no máximo 50'),
+	fullName: z.string().min(5, 'Muito pequeno mínimo 5').max(50, 'Muito grande no máximo 50').transform(stringUtils.capitalizeFirstLetter),
 	email: z.string().email('E-mail invalido '),
 	password: z.string().regex(passwordRegex,'Senha deverá ser forte')
 });
