18d79a188c8490a9c95f844a454a84ce0316f06a
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2023-12-26 11:31:54 -0300

    chore: add anotation

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
 	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		['module-resolver', {
+			root: '.',
+			alias: {
+				'@components': './src/components',
+				'@hooks': './src/hooks',
+				'@screen': './src/screen',
+				'@utils': './src/utils',
+				'@routes': './src/routes'
+			}
+		}]
+	]
 };
+ 
