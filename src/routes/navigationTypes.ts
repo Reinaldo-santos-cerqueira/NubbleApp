a00cf6be94406992b142fcf8b52fac72a3cd4c3d
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-12-27 12:25:56 -0300

    refactor: add export screen props of authroute and approutes

--- src/routes/navigationTypes.ts
+++ src/routes/navigationTypes.ts
@@ -1,9 +1,14 @@
-import { RootStackParams } from './routes';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { AppStackParamsList } from './appStack';
+import { AuthStackParams } from './authStack';
 
 
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace ReactNavigation {
-        interface RootParamList extends RootStackParams {}
+        interface RootParamList extends AuthStackParams, AppStackParamsList { }
     }
 }
+
+export type AppScreenProps<RouteName extends keyof AppStackParamsList> = NativeStackScreenProps<AppStackParamsList, RouteName>
+export type AuthScreenProps<RouteName extends keyof AuthStackParams> = NativeStackScreenProps<AuthStackParams, RouteName>
