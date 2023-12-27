8c7642ceff851e4c3d6c03124d93fa9c510afc7c
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-12-27 12:40:30 -0300

    feat: att project

--- src/screen/app/homeScreen/homeScreen.tsx
+++ src/screen/app/homeScreen/homeScreen.tsx
@@ -11,7 +11,7 @@ export function HomeScreen({ navigation }: AppScreenProps<'HomeScreen'>): React.
 	return (
 		<Screen>
 			<Text preset='headingLarge'>
-				Home Screens
+				Home Screen
 			</Text>
 			<Button title='Settings' onPress={goSettings} />
 		</Screen>
