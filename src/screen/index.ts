661eeac01c8b9dfa4a0ac32977cfa48d6752ef2a
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-12-27 12:25:25 -0300

    feat: add new pages for export

--- src/screen/index.ts
+++ src/screen/index.ts
@@ -2,3 +2,5 @@ export * from './auth/LoginScreen/LoginScreen';
 export * from './auth/forgotPassword/forgotPassword';
 export * from './auth/signUpScreen/signUpScreen';
 export * from './auth/successScreen/successScreen';
+export * from './app/homeScreen/homeScreen';
+export * from './app/settingsScreen/settingsScreen';
