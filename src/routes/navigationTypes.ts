7b476b7f1d3424f93f88af48721cd2d5a7b1b172
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-12 18:16:19 -0300

    feat: created new ScreenProps(TabBottomScreenProps,AppTabScreenProps)

--- src/routes/navigationTypes.ts
+++ src/routes/navigationTypes.ts
@@ -1,8 +1,9 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AppStackParamsList } from './appStack';
 import { AuthStackParams } from './authStack';
-
-
+import { AppTabBottomTabParamsList } from './appTabNavigator';
+import { CompositeScreenProps } from '@react-navigation/native';
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace ReactNavigation {
@@ -11,4 +12,6 @@ declare global {
 }
 
 export type AppScreenProps<RouteName extends keyof AppStackParamsList> = NativeStackScreenProps<AppStackParamsList, RouteName>
+export type TabBottomScreenProps<RouteName extends keyof AppTabBottomTabParamsList> = NativeStackScreenProps<AppTabBottomTabParamsList, RouteName>
 export type AuthScreenProps<RouteName extends keyof AuthStackParams> = NativeStackScreenProps<AuthStackParams, RouteName>
+export type AppTabScreenProps<RouteName extends keyof AppTabBottomTabParamsList> = CompositeScreenProps<BottomTabScreenProps<AppTabBottomTabParamsList, RouteName>, NativeStackScreenProps<AppStackParamsList, 'AppTabNavigator'>>
