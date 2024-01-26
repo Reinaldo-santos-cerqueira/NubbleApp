e1a1cd6eae1e87401aeb12fdcefba3b462fd796d
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-26 15:13:04 -0300

    feat: added more props to homescreen

--- src/screen/app/homeScreen/homeScreen.tsx
+++ src/screen/app/homeScreen/homeScreen.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import { Button, Screen, Text } from '@components';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {
+	useEffect,
+	useState
+} from 'react';
+import {
+	PostItem,
+	Screen
+} from '@components';
 import { AppTabScreenProps } from '@routes';
+import { Post, postService } from '@domain';
+import { FlatList, ListRenderItemInfo } from 'react-native';
+import { HomeHeader } from './components/homeHeader';
 
 export function HomeScreen({ navigation }: AppTabScreenProps<'HomeScreen'>) {
+	const [postList, setPostList] = useState<Post[]>([]);
+
+	useEffect(() => {
+		postService.getList().then((list) => setPostList(list));
+	}, []);
+
+	function renderItem({ item }: ListRenderItemInfo<Post>) {
+		return (
+			<PostItem post={item} />
+		);
+	}
+
 	return (
-		<Screen>
-			<Text preset="headingLarge">Home Screen</Text>
-			<Button
-				title="Settings"
-				onPress={() => navigation.navigate('SettingsScreen')}
+		<Screen style={{ paddingBottom: 0, paddingTop: 0, paddingHorizontal: 0 }}>
+			<FlatList
+				data={postList}
+				keyExtractor={(item) => item.id}
+				renderItem={renderItem}
+				showsVerticalScrollIndicator={false}
+				ListHeaderComponent={<HomeHeader />}
 			/>
 		</Screen>
 	);
