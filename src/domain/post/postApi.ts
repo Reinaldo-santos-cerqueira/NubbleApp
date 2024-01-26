45985a071351f8771ccd47704965eb802639a5fd
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-26 15:09:02 -0300

    feat: added promise

--- src/domain/post/postApi.ts
+++ src/domain/post/postApi.ts
@@ -2,6 +2,9 @@ import { postListMock } from './postListMock';
 import { Post } from './types';
 
 async function getList(): Promise<Post[]> {
+	await new Promise(resolve => setTimeout(() => {
+		resolve('');
+	}, 1000));
 	return postListMock;
 }
 
