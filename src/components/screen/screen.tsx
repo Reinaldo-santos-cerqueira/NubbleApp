948f740d396f290b4694cc3a97163ed48a136880
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-26 15:09:57 -0300

    feat: refactor more props to style and added text components

--- src/components/screen/screen.tsx
+++ src/components/screen/screen.tsx
@@ -1,49 +1,49 @@
 import React from 'react';
-import { Box } from '../box/box';
+import { Box, BoxProps } from '../box/box';
 import { KeyboardAvoidingView, Platform, Pressable, SafeAreaView } from 'react-native';
 import { Icon } from '../icon/icon';
 import { Text } from '../text/text';
 import { ScrollViewContainer, ViewContainer } from './Container';
 import { useNavigation } from '@react-navigation/native';
 import { useAppSafeArea, useAppTheme } from '@hooks';
-interface ScreenProps {
+interface ScreenProps extends BoxProps {
 	canGoBack?: boolean;
 	textBackButton?: string;
 	children: React.ReactNode;
 	scrollable?: boolean;
 }
-export function Screen({ children, canGoBack, textBackButton = '',scrollable}: ScreenProps) {
-	const { top,bottom } = useAppSafeArea();
-	const {colors} = useAppTheme();
+export function Screen({ children, canGoBack, textBackButton = '', scrollable, style, ...boxProps }: ScreenProps) {
+	const { top, bottom } = useAppSafeArea();
+	const { colors } = useAppTheme();
 	const { goBack } = useNavigation();
 	const Container = scrollable ? ScrollViewContainer : ViewContainer;
 	return (
 		<SafeAreaView>
-			<KeyboardAvoidingView 
+			<KeyboardAvoidingView
 				behavior={Platform.OS === 'ios' ? 'padding' : undefined}
 			>
 				<Container backgroundColor={colors.background} >
-					<Box 
-						paddingHorizontal='s24' 
-						paddingBottom='s24' 
-						style={{ paddingTop: top,paddingBottom: bottom }}
+					<Box
+						paddingHorizontal='s24'
+						style={[{ paddingTop: top, paddingBottom: bottom }, style]}
+						{...boxProps}
 					>
 						{
 							canGoBack &&
-							<Pressable onPress={() => console.log('Teste')}>
+							<Pressable onPress={goBack}>
 								<Box mb={'s24'} flexDirection='row' alignItems='center'>
-									<Icon onPress={goBack} name='arrowLeft' color='primary' size={20} />
+									<Icon name='arrowLeft' color='primary' size={20} />
 									{
 										textBackButton !== ''
 										&&
-											<Text
-												ml={'s4'}
-												preset='headingMedium'
-												bold
-												color='grayBlack'
-											>
-												{textBackButton}
-											</Text>
+										<Text
+											ml={'s4'}
+											preset='headingMedium'
+											bold
+											color='grayBlack'
+										>
+											{textBackButton}
+										</Text>
 									}
 								</Box>
 							</Pressable>
