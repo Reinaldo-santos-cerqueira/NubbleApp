6beda72fd24cf2447b1c2d7e492400ae6ca6ba83
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-26 15:10:35 -0300

    feat: configure component text input

--- src/components/textInput/textInput.tsx
+++ src/components/textInput/textInput.tsx
@@ -1,39 +1,39 @@
 import React, { useRef } from 'react';
 import { Pressable, TextInput as RNTextInput, TextInputProps as RnTextInputProps, TextStyle } from 'react-native';
 import { Box, BoxProps } from '../box/box';
-import { $fontFamily, Text,$fontSizes } from '../text/text';
+import { $fontFamily, Text, $fontSizes } from '../text/text';
 import { IconBase } from '../icon/icon';
 import { useAppTheme } from '@hooks';
 
 export interface TextInputProps extends RnTextInputProps {
-    label: string,
+	label: string,
 	erroMessage?: string,
 	icon?: React.ReactElement<IconBase>;
 	boxProps?: BoxProps
 }
 
-export function TextInput({label,erroMessage,icon,boxProps,...props}: TextInputProps){
-	const {colors} = useAppTheme();
+export function TextInput({ label, erroMessage, icon, boxProps, ...props }: TextInputProps) {
+	const { colors } = useAppTheme();
 	const inputRef = useRef<RNTextInput>(null);
-	function focusInput(){
+	function focusInput() {
 		inputRef.current?.focus();
 	}
-	return(
+	return (
 		<Box {...boxProps}>
 			<Pressable
 				onPress={focusInput}
 			>
-				<Text mb={'s4' } color='grayBlack' preset='paragraphMedium'>{label}</Text>
-				<Box 
+				<Text mb={'s4'} color='grayBlack' preset='paragraphMedium'>{label}</Text>
+				<Box
 					{...$textInputContainer(erroMessage)}
 				>
-					<RNTextInput 
+					<RNTextInput
 						ref={inputRef}
 						placeholderTextColor={colors.gray2}
-						{...props} 
+						{...props}
 						style={$textInputStyle}
-					/> 
-					{icon && <Box ml={'s16'}>{icon}</Box> }
+					/>
+					{icon && <Box ml={'s16'}>{icon}</Box>}
 				</Box>
 				{
 					erroMessage && <Text color='error' preset='paragraphSmall' bold>{erroMessage}</Text>
@@ -52,13 +52,13 @@ const $textInputStyle: TextStyle = {
 	...$fontSizes.paragraphMedium
 };
 
-const $textInputContainer =(erroMessage = ''): BoxProps =>{
+const $textInputContainer = (erroMessage = ''): BoxProps => {
 	return {
-		padding:'s16',
-		borderWidth:1,
-		flexDirection:'row',
-		borderColor:erroMessage !== '' ? 'error' :'gray4',
-		borderRadius:'s12'
+		padding: 's16',
+		borderWidth: 1,
+		flexDirection: 'row',
+		borderColor: erroMessage !== '' ? 'error' : 'gray4',
+		borderRadius: 's12'
 	};
 
 };
