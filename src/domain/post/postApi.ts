1188f14e1cf9e3c836c8fd90d475cc205255ecbe
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-02-06 16:06:03 -0300

    feat: added fetch to post on api

--- src/domain/post/postApi.ts
+++ src/domain/post/postApi.ts
@@ -2,12 +2,21 @@ import { postListMock } from './postListMock';
 import { Post } from './types';
 
 async function getList(): Promise<Post[]> {
-	await new Promise(resolve => setTimeout(() => {
-		resolve('');
-	}, 1000));
+	console.log(1);
+
+	const response = await fetch('http://192.168.100.36:3333/user/post', {
+		method: 'GET',
+		headers: {
+			'Authorization': 'Bearer Mw.ZqMDrBCoclDy31Y7lGkIfTS43AntsFHk5rFylYM7zbQGINV4L9YCtHcEptfG'
+		}
+	});
+
+	const data = await response.text();
+	console.log(data);
+
 	return postListMock;
 }
 
 export const postApi = {
 	getList
-};
+};	
